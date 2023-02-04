c45bde073b9fd2a55272753635ef3fab3424c3a1
Shauna845 <shauna10@hotmail.co.uk>
2023-02-04 18:23:28 +0000

    Moved Weather App Project files

---
+++ app.js
@@ -0,0 +1,75 @@
+let now = new Date();
+let h3 = document.querySelector("#date");
+let days = [
+  "Sunday",
+  "Monday",
+  "Tueday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+let day = days[now.getDay()];
+let date = now.getDate();
+let year = now.getFullYear();
+let hour = now.getHours();
+let min = now.getMinutes();
+
+h3.innerHTML = `${day} ${date} ${month}, ${year}, ${hour}:${min}`;
+
+//Week 5//
+let apiKey = "6b137f70277395c99f9b23f929069581";
+
+function displayWeatherCondition(response) {
+  document.querySelector("#cityName").innerHTML = response.data.name;
+  document.querySelector("#temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+
+function searchCity(city) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "6b137f70277395c99f9b23f929069581";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("Belfast");
