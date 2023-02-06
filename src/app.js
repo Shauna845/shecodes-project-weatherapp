933678f6b8593fe9e1720b54fc5cb5b6b078a3d3
Shauna845 <shauna10@hotmail.co.uk>
2023-02-06 19:53:48 +0000

    Updated Api Key

--- src/app.js
+++ src/app.js
@@ -43,7 +43,7 @@ function displayWeatherCondition(response) {
 }
 
 function searchCity(city) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "6b137f70277395c99f9b23f929069581";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
